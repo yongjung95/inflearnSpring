d62392cba739e8f259431dcd8a5789dd3ab80a29
yongjung95 <yongjung95@gmail.com>
2023-07-22 00:15:42 +0900

    :white_check_mark: @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,16 +16,20 @@ public class AppConfig {
 
     @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
