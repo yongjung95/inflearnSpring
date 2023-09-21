88688cf878be510b771b1070ff2c280329ebae4e
yongjung95 <yongjung95@gmail.com>
2023-09-21 23:42:13 +0900

    :white_check_mark: 스프링 MVC 2편 : 연산

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -99,4 +99,11 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
 }
