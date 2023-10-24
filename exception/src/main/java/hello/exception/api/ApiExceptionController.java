3a34e574543b0b9c6642f8b5f0fdb1c420350bb0
yongjung95 <yongjung95@gmail.com>
2023-10-24 10:30:54 +0900

    :white_check_mark: 스프링 MVC 2편 : 예외 처리와 오류페이지 - API 예외 처리(HandlerExceptionResolver 시작)

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -17,6 +17,9 @@ public class ApiExceptionController {
             throw new RuntimeException("잘못된 사용자");
         }
 
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
         return new MemberDto(id, "hello " + id);
     }
 
