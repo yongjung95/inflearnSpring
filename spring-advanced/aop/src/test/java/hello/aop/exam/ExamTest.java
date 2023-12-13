07ce12e19a063ba976b39da9fe3489187e395f0a
yongjung95 <yongjung95@gmail.com>
2023-12-13 17:32:42 +0900

    :white_check_mark: 스프링 핵심 원리 - 고급편 : 스프링 AOP - 실전 예제 [재시도 AOP]

--- spring-advanced/aop/src/test/java/hello/aop/exam/ExamTest.java
+++ spring-advanced/aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 @SpringBootTest
 public class ExamTest {
 
@@ -17,7 +18,7 @@ public class ExamTest {
 
     @Test
     void test() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i < 6; i++) {
             examService.request("data" + i);
         }
     }
