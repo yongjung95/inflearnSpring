104ff4b1faf1deef43208e1ebbc5048d68846dbc
yongjung95 <yongjung95@gmail.com>
2023-12-19 22:26:28 +0900

    :tada: 자바 ORM 표준 JPA 프로그래밍 - 기본편 : JPA 시작하기 프로젝트 생성

---
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HiA");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
