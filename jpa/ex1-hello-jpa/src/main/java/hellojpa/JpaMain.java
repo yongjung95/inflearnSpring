8631c634394fa1779ef55b273f38bb8d063ca9be
yongjung95 <yongjung95@gmail.com>
2023-12-27 16:53:14 +0900

    :white_check_mark: 자바 ORM 표준 JPA 프로그래밍 - 기본편 : 엔티티 매핑

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,16 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            // 영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-
-            System.out.println("================================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
