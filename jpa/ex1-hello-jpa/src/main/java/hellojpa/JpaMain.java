2457cdce5a7b5d1ad114c4e943c6cb25ffc52826
yongjung95 <yongjung95@gmail.com>
2023-12-28 17:43:40 +0900

    :white_check_mark: 자바 ORM 표준 JPA 프로그래밍 - 기본편 :연관관계 매핑 기초 - 단방향 연관관계

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,6 +17,26 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            //팀 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            //회원 저장
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team); //단방향 연관관계 설정, 참조 저장
+
+            em.persist(member);
+
+            //조회
+            Member findMember = em.find(Member.class, member.getId());
+            //참조를 사용해서 연관관계 조회
+            Team findTeam = findMember.getTeam();
+
+            System.out.println("findTeam = " + findTeam.getName());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,32 +2,44 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
-
 @Entity
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name")
-    private String username;
+    @Column(name = "USERNAME")
+    private String name;
 
-    private Integer age;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    public Member() {
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
