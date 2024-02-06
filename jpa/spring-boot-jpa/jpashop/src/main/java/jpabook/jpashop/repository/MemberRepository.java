510f028ae03fac3ea764f9126c311809082f6853
yongjung95 <yongjung95@gmail.com>
2024-02-06 22:51:00 +0900

    :tada: 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 : 회원 도메인 개발

---
+++ jpa/spring-boot-jpa/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em; // 스프링 부트에서만 지원하는 방식이 아니면 안됨. 스프링 부트가 아니라면 를 사용해야 한다.
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
