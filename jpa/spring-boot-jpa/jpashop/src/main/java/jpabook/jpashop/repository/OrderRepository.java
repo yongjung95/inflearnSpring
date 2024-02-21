b40cd4426f4c2749978712dc759a97acf4843110
yongjung95 <yongjung95@gmail.com>
2024-02-21 17:12:02 +0900

    :white_check_mark: 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 : API 개발 고급 - 지연 로딩과 조회 성능 최적화

--- jpa/spring-boot-jpa/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpa/spring-boot-jpa/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,9 +5,8 @@ import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -97,5 +96,13 @@ public class OrderRepository {
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
 
         return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .getResultList();
     }
 }
