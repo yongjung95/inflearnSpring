e99c99975042a1571f9950174bfd1a774d366c6f
yongjung95 <yongjung95@gmail.com>
2024-03-12 21:51:34 +0900

    :white_check_mark: 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 : API 개발 고급 - 컬렉션 조회 최적화 - 주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- jpa/spring-boot-jpa/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpa/spring-boot-jpa/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -85,9 +85,24 @@ public class OrderApiController {
         return result;
     }
 
+    /**
+     * 컬렉션은 별도로 조회
+     * Query: 루트 1번, 컬렉션 N 번
+     * 단건 조회에서 많이 사용하는 방식
+     */
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    /**
+     * 최적화
+     * Query: 루트 1번, 컬렉션 1번
+     * 데이터를 한꺼번에 처리할 때 많이 사용하는 방식
+     */
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
     }
 
     @Data
