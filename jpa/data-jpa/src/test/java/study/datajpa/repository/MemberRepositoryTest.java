22de6fcad158ec202618bf814c9fc6fe400c8b2a
yongjung95 <yongjung95@gmail.com>
2024-03-29 22:44:00 +0900

    :white_check_mark: 실전! 스프링 데이터 JPA : 04. 쿼리 메소드 기능 - 반환 타입

--- jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -150,5 +151,17 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Optional<Member> findMember = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("findMember = " + findMember);
     }
 }
