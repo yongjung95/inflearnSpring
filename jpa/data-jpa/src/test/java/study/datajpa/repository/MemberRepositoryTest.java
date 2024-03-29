91fe7c1b944418c7921c3a948ff0892e8aa44215
yongjung95 <yongjung95@gmail.com>
2024-03-29 23:54:13 +0900

    :white_check_mark: 실전! 스프링 데이터 JPA : 04. 쿼리 메소드 기능 - @EntityGraph

--- jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -224,4 +224,29 @@ class MemberRepositoryTest {
         // then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        memberRepository.save(new Member("member1", 10, teamA));
+        memberRepository.save(new Member("member1", 20, teamB));
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+    }
 }
