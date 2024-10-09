9409c386610cceaa7808345da0451720164121d9
yongjung95 <yongjung95@gmail.com>
2024-10-09 19:56:34 +0900

    :white_check_mark: 고급 1편, 멀티스레드와 동시성 : 02. 스레드 생성과 실행 - 스레드 생성 Runnable

---
+++ advanced-java/java-adv1/src/thread/start/HelloRunnable.java
@@ -0,0 +1,10 @@
+package thread.start;
+
+public class HelloRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": run()");
+    }
+}
+
