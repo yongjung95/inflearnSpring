3f1dba16863cf5aa5e6bb329742b20cb49cab317
yongjung95 <yongjung95@gmail.com>
2024-10-14 18:57:05 +0900

    :white_check_mark: 고급 1편, 멀티스레드와 동시성 : 03. 스레드 제어와 생명 주기1 - 프린터 예제

---
+++ advanced-java/java-adv1/src/thread/control/printer/MyPrinterV3.java
@@ -0,0 +1,55 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+
+public class MyPrinterV3 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+        
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            System.out.println("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printerThread.interrupt();
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                    Thread.sleep(3000); //출력에 걸리는 시간
+                    log("출력 완료: " + job);
+                } catch (InterruptedException e) {
+                    log("인터럽트!");
+                    break;
+                }
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
