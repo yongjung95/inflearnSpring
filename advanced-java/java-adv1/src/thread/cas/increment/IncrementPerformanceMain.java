0e9fc5995df8ea8dd3d7e24d3d26d911d05957df
yongjung95 <yongjung95@gmail.com>
2024-10-23 06:28:08 +0900

    :white_check_mark: 고급 1편, 멀티스레드와 동시성 : 10. CAS - 동기화와 원자적 연산 진행 중

---
+++ advanced-java/java-adv1/src/thread/cas/increment/IncrementPerformanceMain.java
@@ -0,0 +1,22 @@
+package thread.cas.increment;
+
+public class IncrementPerformanceMain {
+
+    public static final long COUNT = 100_000_000;
+
+    public static void main(String[] args) {
+        test(new BasicInteger());
+        test(new VolatileInteger());
+        test(new SynchronizedInteger());
+        test(new MyAtomicInteger());
+    }
+
+    private static void test(IncrementInteger incrementInteger) {
+        long startMs = System.currentTimeMillis();
+        for (long i = 0; i < COUNT; i++) {
+            incrementInteger.increment();
+        }
+        long endMs = System.currentTimeMillis();
+        System.out.println(incrementInteger.getClass().getSimpleName() + ": ms= " + (endMs - startMs));
+    }
+}
