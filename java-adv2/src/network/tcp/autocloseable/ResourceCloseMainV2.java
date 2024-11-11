b9ad70f8fdc00ac19b522b66498410d73430d1f4
yongjung95 <yongjung95@gmail.com>
2024-11-11 19:00:45 +0900

    :white_check_mark: 김영한의 실전 자바 - 고급 2편, I/O, 네트워크, 리플렉션 : 7. 네트워크 - 프로그램1

---
+++ java-adv2/src/network/tcp/autocloseable/ResourceCloseMainV2.java
@@ -0,0 +1,39 @@
+package network.tcp.autocloseable;
+
+public class ResourceCloseMainV2 {
+
+    public static void main(String[] args) {
+        try {
+            logic();
+        } catch (CallException e) {
+            System.out.println("CallException 예외 처리");
+            e.printStackTrace();
+        } catch (CloseException e) {
+            System.out.println("CloseException 예외 처리");
+            e.printStackTrace();
+        }
+    }
+
+    private static void logic() throws CallException, CloseException {
+        ResourceV1 resource1 = null;
+        ResourceV1 resource2 = null;
+
+        try {
+            resource1 = new ResourceV1("resource1");
+            resource2 = new ResourceV1("resource2");
+
+            resource1.call();
+            resource2.callEx(); // CallException
+        } catch (CallException e) {
+            System.out.println("ex: " + e);
+            throw e; // CallException 다시 던짐
+        } finally {
+            if (resource2 != null) {
+                resource2.closeEx(); // CloseException 발생!
+            }
+            if (resource1 != null) {
+                resource1.closeEx(); // 이 코드 호출 안됨!
+            }
+        }
+    }
+}
---
+++ java-adv2/src/network/tcp/autocloseable/ResourceCloseMainV3.java
@@ -0,0 +1,48 @@
+package network.tcp.autocloseable;
+
+public class ResourceCloseMainV3 {
+
+    public static void main(String[] args) {
+        try {
+            logic();
+        } catch (CallException e) {
+            System.out.println("CallException 예외 처리");
+            e.printStackTrace();
+        } catch (CloseException e) {
+            System.out.println("CloseException 예외 처리");
+            e.printStackTrace();
+        }
+    }
+
+    private static void logic() throws CallException, CloseException {
+        ResourceV1 resource1 = null;
+        ResourceV1 resource2 = null;
+
+        try {
+            resource1 = new ResourceV1("resource1");
+            resource2 = new ResourceV1("resource2");
+
+            resource1.call();
+            resource2.callEx(); // CallException
+        } catch (CallException e) {
+            System.out.println("ex: " + e);
+            throw e; // CallException 다시 던짐
+        } finally {
+            if (resource2 != null) {
+                try {
+                    resource2.closeEx();
+                } catch (CloseException e) {
+                    // close()에서 발생한 예외는 버린다. 필요하면 로깅 정도
+                    System.out.println("close ex: " + e);
+                }
+            }
+            if (resource1 != null) {
+                try {
+                    resource1.closeEx();
+                } catch (CloseException e) {
+                    System.out.println("close ex: " + e);
+                }
+            }
+        }
+    }
+}
